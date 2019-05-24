6e36f0f8446455057a45504a24b87c115faf31b4
avivsab <avivsab19@gmail.com>
2019-05-24 17:30:00 +0300

    Add heros data file to simulate remote data

---
+++ ngHeroes/src/app/data-heroes.ts
@@ -0,0 +1,15 @@
+import Hero from './hero';
+
+export const HerosData: Hero[] = [
+    {id:11, name: 'Mr Machluf'},
+    {id:12, name: 'Dr Fisher'},
+    {id: 13, name: 'Joh'},
+    {id: 14, name: 'Jean'},
+    {id: 15, name: 'Magneta'},
+    { id: 16, name: 'RubberMan' },
+    { id: 17, name: 'Dynama' },
+    { id: 18, name: 'Dr IQ' },
+    { id: 19, name: 'Magma' },
+    { id: 20, name: 'Tornado' }
+
+];
