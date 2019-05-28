3142c42451d2f296a7a88e43c4c7695ac947e7ed
avivsab <avivsab19@gmail.com>
2019-05-28 23:05:39 +0300

    Add routing module for route to /heroes and present HeroComp

---
+++ ngHeroes/src/app/app-routing.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+// import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
+import { HeroesComponent } from './heroes/heroes.component';
+
+const routes: Routes = [
+  {path: 'heroes', component: HeroesComponent}
+];
+
+@NgModule({
+  imports: [
+    // CommonModule
+  ],
+  // declarations: []// for comp - usually not in use
+
+  exports: [RouterModule]
+
+})
+
+export class AppRoutingModule { }
