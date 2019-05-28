24112c87321fa28e6961f92214fd70ae593e8734
avivsab <avivsab19@gmail.com>
2019-05-28 19:14:24 +0300

    replace asignment to heroes arr to subscribe after change heroService to Observ

--- ngHeroes/src/app/heroes/heroes.component.ts
+++ ngHeroes/src/app/heroes/heroes.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
 import { HeroService } from '../hero.service';
+import { HeroesData } from '../data-heroes';
 
 @Component({
   selector: 'app-heroes',
@@ -17,7 +18,7 @@ export class HeroesComponent implements OnInit {
   
   constructor(private heroService: HeroService) { }
   getHeros(): void {
-    this.heroes = this.heroService.getHeroes()
+    this.heroService.getHeroes().subscribe(heroes => this.heroes = heroes)
   }
   ngOnInit() {
     this.getHeros();
