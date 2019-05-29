a76f45afe083b500f3f04249fd16ebec3168f6ba
avivsab <avivsab19@gmail.com>
2019-05-29 10:57:10 +0300

    Add private property heroesURL and change method getHeroes to recive data from fake api

--- ngHeroes/src/app/hero.service.ts
+++ ngHeroes/src/app/hero.service.ts
@@ -3,19 +3,23 @@ import { Hero } from './hero';
 import { HeroesData } from './data-heroes';
 import { Observable, of } from 'rxjs';
 import { MessageService } from './message.service';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 @Injectable({
   providedIn: 'root'
 })
 export class HeroService {
 
-  constructor(private messageService: MessageService) { }
+  constructor(private http: HttpClient,private messageService: MessageService) { }
 
   getHeroes(): Observable<Hero[]>{
-    this.messageService.add('HeroService fetched heroes');
-    return of(HeroesData); 
+    return this.http.get<Hero[]>(this.heroesURL); 
   }
   getHero(id: number): Observable<Hero> {
     this.messageService.add(`HeroService: fetched hero id=${id}`);
     return of(HeroesData.find(hero => hero.id===id));
   } 
+  private log(message: string) {
+    this.messageService.add(`HeroService: ${message}`);
+  }
+  private heroesURL = 'api/heroes';
 }
