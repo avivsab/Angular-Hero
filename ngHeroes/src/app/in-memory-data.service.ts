42b321065f193663c9103cafdd737bc526d7efcf
avivsab <avivsab19@gmail.com>
2019-05-29 10:21:15 +0300

    Add method (change func name from gen to generateId) that return num using terranery, spread, Math.max & map

--- ngHeroes/src/app/in-memory-data.service.ts
+++ ngHeroes/src/app/in-memory-data.service.ts
@@ -9,17 +9,20 @@ export class InMemoryDataService implements InMemoryDataService {
 
   createDb() {
     const heroes = [
-      { id: 11, name: 'Dr Nice' },
-      { id: 12, name: 'Narco' },
-      { id: 13, name: 'Bombasto' },
-      { id: 14, name: 'Celeritas' },
-      { id: 15, name: 'Magneta' },
-      { id: 16, name: 'RubberMan' },
-      { id: 17, name: 'Dynama' },
-      { id: 18, name: 'Dr IQ' },
-      { id: 19, name: 'Magma' },
-      { id: 20, name: 'Tornado' }
+    {id:11, name: 'Mr Machluf'},
+    {id:12, name: 'Dr Fisher'},
+    {id: 13, name: 'Joh'},
+    {id: 14, name: 'Jean'},
+    {id: 15, name: 'Magneta'},
+    { id: 16, name: 'RubberMan' },
+    { id: 17, name: 'Dynama' },
+    { id: 18, name: 'Dr IQ' },
+    { id: 19, name: 'Magma' },
+    { id: 20, name: 'Tornado' }
     ];
     return {heroes}
+  }
+  generateId(heroes: Hero[]): number {
+    return heroes.length > 0 ? Math.max(...heroes.map(hero => hero.id)) + 1 : 11;
   }
 }
