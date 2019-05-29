ac620b4e2cd36ef5fa79b572494ba6e8eefc3000
avivsab <avivsab19@gmail.com>
2019-05-29 11:13:47 +0300

    Add error handler from rxjs/operators, add pipe to catch err and private meth hadleError to relate the pipe and to send message to console

--- ngHeroes/src/app/hero.service.ts
+++ ngHeroes/src/app/hero.service.ts
@@ -4,6 +4,7 @@ import { HeroesData } from './data-heroes';
 import { Observable, of } from 'rxjs';
 import { MessageService } from './message.service';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { catchError, map, tap } from 'rxjs/operators';
 @Injectable({
   providedIn: 'root'
 })
@@ -12,7 +13,7 @@ export class HeroService {
   constructor(private http: HttpClient,private messageService: MessageService) { }
 
   getHeroes(): Observable<Hero[]>{
-    return this.http.get<Hero[]>(this.heroesURL); 
+    return this.http.get<Hero[]>(this.heroesURL).pipe(catchError(this.handleError<Hero[]>('getHeroes', []))) 
   }
   getHero(id: number): Observable<Hero> {
     this.messageService.add(`HeroService: fetched hero id=${id}`);
@@ -22,4 +23,18 @@ export class HeroService {
     this.messageService.add(`HeroService: ${message}`);
   }
   private heroesURL = 'api/heroes';
+
+  private handleError<T> (operation = 'operation', result?: T) {
+    return (error: any): Observable<T> => {
+   
+      // TODO: send the error to remote logging infrastructure
+      console.error(error); // log to console instead
+   
+      // TODO: better job of transforming error for user consumption
+      this.log(`${operation} failed: ${error.message}`);
+   
+      // Let the app keep running by returning an empty result.
+      return of(result as T);
+    };
+  }
 }
